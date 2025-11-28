92928d17be7f2a25db3cdc2efd11feddef314b45
Zeron <dotdot20072607@gmail.com>
2025-11-28 07:25:27 +0500

    Task4

---
+++ Tyuiu.LyapinSE.Sprint5.Task4.V18.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.LyapinSE.Sprint5.Task4.V18.Lib
+{
+    public class DataService : ISprint5Task4V18
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string data = File.ReadAllText(path);
+            double x = double.Parse(data);
+            double y = Math.Cos(x) + Math.Pow(x, 2) / 2.0;
+            return Math.Round(y, 3);
+        }
+    }
+}
