e408f49a12437e709a1cbe0d7aeed91454196ede
Zeron <dotdot20072607@gmail.com>
2025-11-28 06:05:20 +0500

    Add project files.

---
+++ Tyuiu.LyapinSE.Sprint5.Task0.V20.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.LyapinSE.Sprint5.Task0.V20.Lib
+{
+    public class DataService : ISprint5Task0V20
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double result = 2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + 4.1 * x * 2;
+            result = Math.Round(result, 3);
+            File.WriteAllText(path, Convert.ToString(result));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.LyapinSE.Sprint5.Task0.V20.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.LyapinSE.Sprint5.Task0.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
